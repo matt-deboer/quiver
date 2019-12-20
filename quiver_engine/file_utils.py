25d037f3dedf9899dc9f4d3f3478507ae349decf
Matt DeBoer <matt.deboer@gmail.com>
2019-12-20 12:04:06 -0800

    replace imsave with imageio.write

--- quiver_engine/file_utils.py
+++ quiver_engine/file_utils.py
@@ -2,12 +2,12 @@ import re
 from os.path import relpath, abspath
 from os import listdir
 
-from scipy.misc import imsave
+import imageio
 from quiver_engine.util import deprocess_image
 
 def save_layer_img(layer_outputs, layer_name, idx, temp_folder, input_path):
     filename = get_output_filename(layer_name, idx, temp_folder, input_path)
-    imsave(filename, deprocess_image(layer_outputs))
+    imageio.write(filename, deprocess_image(layer_outputs))
     return relpath(filename, abspath(temp_folder))
 
 def get_output_filename(layer_name, z_idx, temp_folder, input_path):
